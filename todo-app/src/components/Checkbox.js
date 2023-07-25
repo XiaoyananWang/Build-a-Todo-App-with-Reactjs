d6d738206c71d417d2cd4dcb56e0c59fafa8d14f
wxyn <100791680+XiaoyananWang@users.noreply.github.com>
2023-07-25 13:09:18 -0400

    adding a nice animation line when a task is marked as done

--- todo-app/src/components/Checkbox.js
+++ todo-app/src/components/Checkbox.js
@@ -1,4 +1,3 @@
-
 //checked = false: This is a default value for the checked prop. If the checked prop is not provided when the Checkbox component is used, it will default to false.
 function Checkbox({ checked = false, onClick }) {
   //we can get rid of this state and make the component a controlled component
@@ -7,12 +6,8 @@ function Checkbox({ checked = false, onClick }) {
     <div onClick={onClick}>
       {!checked && (
         <div className="checkedbox unchecked">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 448 512"
-          >
-            <path d="M0 96C0 60.7 28.7 32 64 32H384c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96z" />
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M384 32C419.3 32 448 60.65 448 96V416C448 451.3 419.3 480 384 480H64C28.65 480 0 451.3 0 416V96C0 60.65 28.65 32 64 32H384zM384 80H64C55.16 80 48 87.16 48 96V416C48 424.8 55.16 432 64 432H384C392.8 432 400 424.8 400 416V96C400 87.16 392.8 80 384 80z" />
           </svg>
         </div>
       )}
--- todo-app/src/components/Task.js
+++ todo-app/src/components/Task.js
@@ -3,9 +3,11 @@ import Checkbox from "./Checkbox";
 function Task({ name, done, onToggle }) {
   //the newDone will be the opposite of done, so !done
   return (
-    <div className="task">
+    //adding extra className to this div to add the crossing line to the task if it's done
+    //To add an animation to the linn-through, you can add a span element first to the name
+    <div className={"task " + (done ? "done" : "")}>
       <Checkbox checked={done} onClick={() => onToggle(!done)} />
-      {name}
+      <span>{name}</span>
     </div>
   );
 }
